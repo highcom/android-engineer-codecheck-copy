e28d389e7c449e3458665feccdcedef486eaee2d
koichi highcom <youhappywith@yahoo.co.jp>
2022-01-13 23:43:31 +0900

    テストを追加  - UITestを追加

---
+++ app/src/androidTest/kotlin/jp/co/yumemi/android/code_check/SearchRepositoryMainActivityTest.kt
@@ -0,0 +1,101 @@
+package jp.co.yumemi.android.code_check
+
+
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import android.view.View
+import android.view.ViewGroup
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+
+import org.hamcrest.CoreMatchers.allOf
+
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.hamcrest.core.Is.`is`
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class SearchRepositoryMainActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(SearchRepositoryMainActivity::class.java)
+
+    @Test
+    fun searchRepositoryMainActivityTest() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        Thread.sleep(500)
+
+        val textInputEditText = onView(
+            allOf(
+                withId(R.id.searchInputText),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.searchInputLayout),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText.perform(replaceText("test"), closeSoftKeyboard())
+
+        val textInputEditText2 = onView(
+            allOf(
+                withId(R.id.searchInputText), withText("test"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.searchInputLayout),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText2.perform(pressImeActionButton())
+
+        val recyclerView = onView(
+            allOf(
+                withId(R.id.recyclerView),
+                childAtPosition(
+                    withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                    1
+                )
+            )
+        )
+        recyclerView.perform(actionOnItemAtPosition<ViewHolder>(0, click()))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
